fd508d9978cf7b93ccc90232f18bdcc6f0b099ec
jeff <jeffdelmotte@gmail.com>
2024-10-02 17:01:11 +0200

    intégration de la gallery

--- FrontEnd/src/api.js
+++ FrontEnd/src/api.js
@@ -2,6 +2,20 @@ async function getCategories(){
     const url='http://localhost:5678/api/categories';
     const res= await fetch(url);
     const categories= await res.json();
+   
 
     return categories;
+}
+
+async function getWorks(){
+    const url='http://localhost:5678/api/works';
+    const res= await fetch(url);
+    const works= await res.json();
+    // for (let i=0; i<json.length; index ++); {
+    //     setImages(json[i]);
+    // }
+    setImages(json);
+
+        
+    return works;
 }
--- FrontEnd/src/main.js
+++ FrontEnd/src/main.js
@@ -3,4 +3,24 @@ createButtons();
 async function createButtons(){
     const categories = await getCategories();
     console.log(categories);
+       
 }
+
+createGallery();
+
+async function createGallery() { 
+const works = await getWorks();
+console.log(works);
+
+}
+
+function setImages(data) {
+const figure = document.createElement("figure");
+figure.innerHTML = '<img src=${data[i].imageUrl} alt=${data[i].title}><figcaption>${data[i].title}</figcaption>';
+
+document.body.append(figure);
+}
+        
+
+
+
