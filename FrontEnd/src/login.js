fe04b1e37df9ff741d61bab1b4b018aec8b22087
jeff <jeffdelmotte@gmail.com>
2024-10-09 16:08:23 +0200

    intégration du message d'erreur

--- FrontEnd/src/login.js
+++ FrontEnd/src/login.js
@@ -6,10 +6,26 @@ async function submitId(event) {
         email: document.getElementById('email').value,
         password: document.getElementById('password').value,
     };
+
     let result = await postLogin(credentials);
     console.log(result);
     console.log("E-mail:", credentials.email);
     console.log("Mot de passe:", credentials.password);
+    if (result.status === 200) {
+        window.location.href = "index.html";
+
+    }
+    else {
+       errorMessage();
+    }
+    
+    function errorMessage () {
+        const messageErrorBox = document.createElement("div");
+        messageErrorBox.className="error-box";
+        messageErrorBox.innerHTML = "E-mail ou mot de passe invalide";
+        document.querySelector(".user-login").prepend(messageErrorBox);
+        
+    }
     
 
     
