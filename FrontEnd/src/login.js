9981f80201eaec3bcdbe17a83b85995c894fb8ec
jeff <jeffdelmotte@gmail.com>
2024-10-12 11:35:22 +0200

    admin mode

--- FrontEnd/src/login.js
+++ FrontEnd/src/login.js
@@ -43,3 +43,26 @@ function errorMessage() {
 
 
 }
+
+function displayAdminMode() {
+    if (localStorage.getItem("token")) {
+        const editBanner = document.createElement("div");
+        editBanner.className ="edit";
+        editBanner.innerHTML = `<p><a href="#modal1" class="js-modal"><i class="fa-regular fa-pen-to-square"></i>Mode édition</a></p>`;
+        document.body.prepend(editBanner);
+    }
+
+}
+
+displayAdminMode();
+
+const openModal = function(e) {
+    e.preventDefault();
+    const target = document.querySelector(e.target.getAttribute("href"));
+    target.style.display = "block";
+    target.removeAttribute("aria-hidden");
+    target.setAttribute("aria-modal", "true");
+}
+document.querySelectorAll(".js-modal").forEach(a => {
+    a.addEventListener("click", openModal);
+});
