6a089fa93686676a68a755b1a311153660d9be8c
jeff <jeffdelmotte@gmail.com>
2024-10-14 10:24:21 +0200

    gestion du lien modifier

--- FrontEnd/src/main.js
+++ FrontEnd/src/main.js
@@ -59,8 +59,10 @@ function isUserLoggedIn() {
 
 function displayLoginBanner() {
   const banner = document.getElementById("loginBanner");
+  const modify = document.querySelector(".modify");
   if (isUserLoggedIn()) {
     banner.style.display = "block";
+    modify.style.display = "block";
   }
 }
 
@@ -92,3 +94,33 @@ document.addEventListener("DOMContentLoaded", function () {
     console.log("utilisateur non connecté, affichage login");
   }
 });
+
+const openModal = function (e) {
+  e.preventDefault(); 
+  const targetElement = e.target.closest('a');
+  if (targetElement) {
+    const target = document.querySelector(targetElement.getAttribute("href"));  
+    if (target) {
+      target.style.display = "flex";   
+      target.removeAttribute("aria-hidden");      
+    }
+  }
+};
+
+ 
+document.querySelectorAll(".js-modal").forEach(a => {
+  a.addEventListener("click", openModal);
+});
+
+const closeModal = function (e) {
+  const modal = e.target.closest('.modal');  // Trouver la modale parente
+  if (modal) {
+    modal.style.display = "none";  // Masquer la modale
+    modal.setAttribute("aria-hidden", "true");  // Accessibilité : masquer la modale
+  }
+};
+
+document.querySelectorAll(".close-modal").forEach(button => {
+  button.addEventListener("click", closeModal);
+});
+
